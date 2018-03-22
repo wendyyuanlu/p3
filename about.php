2258e0ad8974b4c02ee54f9c98a33cb511b269e7
Unknown <wendyyuanlu@gmail.com>
2018-03-21 22:43:14 -0700

    updated bio

--- about.php
+++ about.php
@@ -8,10 +8,10 @@
           <section class="grid grid-center">
             <div class="col-2">
               <p>
-                I'm Wendy, a UX designer from Vancouver, Canada. I am passionate about creating insight-driven work to help reduce the frictions in people's daily lives. In addition to designing workflows and interfaces, I enjoy prototyping, conducting usability studies, and cross-team collaborations. My previous experiences include working at Microsoft and SAP, as well as mid-sized startups and non-profit organizations.
+                I'm Wendy, a UX designer from Vancouver, Canada. I am passionate about creating insight-driven work to help reduce the frictions in people's daily lives. In addition to designing workflows and interfaces, I enjoy prototyping, conducting usability studies, and cross-team collaborations. My previous experiences include working at Microsoft and <a href="conversations.php">SAP</a>, as well as mid-sized startups and non-profit organizations.
               </p>
               <p>
-                My mini origin story: I started web designing at the age of 11, studied graphic design in highschool, and am currently wrapping up a degree in design at Simon Fraser University. My career in design was jumpstarted by a childhood passion, and I can't wait to see where this path will take me.
+                I am currently wrapping up a degree in design at Simon Fraser University. In my down time, you can find me chasing my passion in <a href="">photography</a>.
               </p>
 
               <p>Let's get in touch! Feel free to shoot me an <a href="mailto:heywendylu@gmail.com">e-mail</a> or connect with me on <a href="https://www.linkedin.com/in/heywendylu/" onclick="target='_blank'">LinkedIn</a>.</p>
