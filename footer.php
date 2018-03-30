833d0d086c0fed92486a75d05f54be8ca1711d25
Unknown <wendyyuanlu@gmail.com>
2018-03-30 02:17:49 -0700

    Added social media links and IE stylesheet

--- footer.php
+++ footer.php
@@ -11,9 +11,9 @@
 
       <footer>
         <p>
-          <a href="https://www.linkedin.com/in/heywendylu" onclick="target='_blank'"><img src="img/linkedin.svg" alt="Facebook" height="16px" width="16px"></a> &nbsp;&nbsp;
-          <a href="http://flickr.com/photos/heywendylu" onclick="target='_blank'"><img src="img/flickr.svg" alt="Flickr" height="16px" width="16px"></a> &nbsp;&nbsp;
-          <a href="http://instagram.com/heywendylu" onclick="target='_blank'"><img src="img/instagram.svg" alt="Instagram" height="16px" width="16px"></a>
+          <a href="https://www.linkedin.com/in/heywendylu" onclick="target='_blank'"><img src="img/li.svg" alt="Wendy's LinkedIn" height="16px" width="16px"></a> &nbsp;&nbsp;
+          <a href="http://flickr.com/photos/heywendylu" onclick="target='_blank'"><img src="img/flickr.svg" alt="Wendy's Flickr" height="16px" width="16px"></a> &nbsp;&nbsp;
+          <a href="http://instagram.com/heywendylu" onclick="target='_blank'"><img src="img/instagram.svg" alt="Wendy's Instagram" height="16px" width="16px"></a>
         </p>
         <p><a href="styleguide.php">Styleguide</a> &nbsp;&nbsp; | &nbsp;&nbsp; <a href="citations.php">Citations</a> &nbsp;&nbsp; | &nbsp;&nbsp; <a href="https://github.com/wendyyuanlu/p3" onclick="target='_blank'">Github</a></p>
         <p>&copy; 2018 Wendy Lu. All rights reserved.</p>
