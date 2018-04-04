93bbafea0013ea6e5403397ec91ab35390ed99cd
Unknown <wendyyuanlu@gmail.com>
2018-04-04 11:40:59 -0700

    Updated citations

--- citations.php
+++ citations.php
@@ -4,7 +4,10 @@
 
           <p>Adams, V. (n.d.). <em>Muli.</em> Retrieved from https://fonts.google.com/specimen/Muli</p>
           <p>Diogo, P. (2015, June 16). <em>Get the browser viewport dimensions with JavaScript.</em> Retrieved from https://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa</p>
+          <p>Hernandez, M. (2016, February 3). <em>Pro Tip: How to Write Conditional CSS for IE10 and 11.</em> Retrieved from https://www.mediacurrent.com/blog/pro-tip-how-write-conditional-css-ie10-and-11/</p>
+          <p>Instagram. (n.d.). <em>Assets.</em> Retrieved from https://en.instagram-brand.com/assets
           <p>Khan, M. (2018, February 13). <em>Hover effect : expand bottom border.</em> Retrieved from https://stackoverflow.com/questions/28623446/hover-effect-expand-bottom-border</p>
+          <p>LinkedIn. (n.d.). <em>Downloads.</em> Retrieved from https://brand.linkedin.com/downloads</p>
           <p>Marquizzo (2017, May 19). <em>How to get height of entire document with JavaScript?</em> Retrieved from https://stackoverflow.com/questions/1145850/how-to-get-height-of-entire-document-with-javascript</p>
           <p>Sorensen, C. (n.d.). <em>Playfair Display.</em> Retrieved from https://fonts.google.com/specimen/Playfair+Display</p>
           <p>W3schools (n.d.). <em>HTML DOM scrollTop Property.</em> Retrieved from https://www.w3schools.com/jsreF/tryit.asp?filename=tryjsref_onscroll3</p>
